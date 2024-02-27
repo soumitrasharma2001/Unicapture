288b2f65741a77ed4ae27a8fd0a7c5b13b3a1ade
Soumitra Sharma <100134821+soumitrasharma2001@users.noreply.github.com>
2024-02-27 12:43:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ page1Content.addEventListener("mousemove", (dets) => {
     cursor.style.left = dets.x + "px"
     cursor.style.top = dets.y + "px"
 })
+let intro = document.getElementById('intro')
+let main = document.getElementById('main')
+setTimeout(() => {
+    intro.style.display = "none"
+    main.style.display = "block"
+}, 7000)
