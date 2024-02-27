724e34a1e72bc5b9c8dee682c61bc0a33208d0f6
Ujjwal Pawar <103569478+Ujjwalpawar12345@users.noreply.github.com>
2024-02-27 17:55:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
+function cursorEffect(){
 let page1Content = document.querySelector("#page1-content")
 let cursor = document.querySelector("#cursor")
 page1Content.addEventListener("mousemove", (dets) => {
-    cursor.style.left = dets.x + "px"
-    cursor.style.top = dets.y + "px"
+   gsap.to(cursor,{
+    x:dets.x,
+    y:dets.y 
+   })
 })
+page1Content.addEventListener("mouseenter",function(){
+    gsap.to(cursor,{
+        scale:1,
+        opacity:1
+    })
+})
+page1Content.addEventListener("mouseleave",function(){
+    gsap.to(cursor,{
+        scale:0,
+        opacity:0
+    })
+})
+}
+cursorEffect()
 let intro = document.getElementById('intro')
 let main = document.getElementById('main')
 setTimeout(() => {
     intro.style.display = "none"
     main.style.display = "block"
 }, 7000)
+document.addEventListener("DOMContentLoaded", function() {
+    const texts = document.querySelectorAll(".text");
+  
+    texts.forEach((text, index) => {
+      const delay = text.getAttribute("data-delay");
+      text.style.animationDelay = delay;
+    });
+  });
+  
